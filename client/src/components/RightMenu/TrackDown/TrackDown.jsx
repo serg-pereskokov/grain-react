3756862c37586e8ec7d2250e521d8fa442db9bca
Sergey Pereskokov <serg.pereskokov@gmail.com>
2021-12-06 22:16:07 +0200

    style TrackDown

--- client/src/components/RightMenu/TrackDown/TrackDown.jsx
+++ client/src/components/RightMenu/TrackDown/TrackDown.jsx
@@ -3,18 +3,54 @@ import styles from './TrackDown.module.scss'
 import allCars from '../../../assets/trackDownSettings.json'
 
 const TrackDown = () => {
+    
+    const {agricultural_machinery, cars} = allCars
 
-    console.log(allCars);
+    const Explorer = () => {
+        return (
+            <ul className={styles.mainFolder}>
+                <li>
+                    <span className={styles.explorerMainDescr}>Все машины</span>
+                    <ul>
+                        <li>
+                            <span className={styles.explorerDescr}>СХ техника</span>
+                            <ul>
+                                {
+                                    agricultural_machinery.map((item, index) => {
+                                        return (
+                                            <li key={index}>{item}</li>
+                                        )
+                                    })
+                                }
+                            </ul>
+                        </li>
+                        <li>
+                            <span className={styles.explorerDescr}>Машины</span>
+                            <ul>
+                                {
+                                    cars.map((item, index) => {
+                                        return (
+                                            <li key={index}>{item}</li>
+                                        )
+                                    })
+                                }
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        )
+    }
 
     return (
         <div className={styles.TrackDown}>
             <div className={styles.header}>Выбор техники</div>
             <div className={styles.searchBlock}>
-                <input type="search" placeholder='Поиск'/>
+                <input type="text" placeholder='Поиск' className={styles.search}/>
                 <i className='material-icons'>search</i>
             </div>
             <div className={styles.explorer}>
-                1
+                <Explorer />
             </div>
         </div>
     )
